2f3f3486d5da1be7b7cbfd9076200c551cfd3956
Javed Shaik <shaikjaved1228@gmail.com>
2022-09-15 12:05:32 +0530

    added input verification

--- CustomConsole.cpp
+++ CustomConsole.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <string>
+#include <limits>
 using namespace std;
 
 #include "CustomConsole.h"
@@ -15,6 +16,12 @@ using namespace std;
 	{
 		float f;
 		cin >> f;
+		while(cin.fail()){
+			cout<<"Please enter a float value: ";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(),'\n');
+			cin >> f;
+		}
 		return f;
 	}
 
@@ -22,6 +29,13 @@ using namespace std;
 	{
 		double d;
 		cin >> d;
+		while(cin.fail()){
+			cout<<"Please enter a Double: ";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(),'\n');
+			cin >> d;
+		}
+
 		return d;
 	}
 
@@ -29,6 +43,13 @@ using namespace std;
 	{
 		int i;
 		cin >> i;
+		while(cin.fail()){
+			cout<<"Please enter an Integer: ";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(),'\n');
+			cin >> i;
+		}
+
 		return i;
 	}
 
@@ -36,6 +57,12 @@ using namespace std;
 	{
 		char c;
 		cin >> c;
+		while(cin.fail()){
+			cout<<"Please enter a single character: ";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(),'\n');
+			cin >> c;
+		}
 		return c;
 	}
 	
@@ -43,5 +70,11 @@ using namespace std;
 	{
 		long l;
 		cin >> l;
+		while(cin.fail()){
+			cout<<"Please enter a long int value: ";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(),'\n');
+			cin >> l;
+		}
 		return l;
 	}
